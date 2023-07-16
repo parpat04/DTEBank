ae522775c1cea332ccb3b756f92f89275b3267f6
parpat04 <parthivpatel46143@gmail.com>
2023-07-16 14:26:06 -0400

    dte bank

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import "./styles.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { faBuildingColumns } from "@fortawesome/free-solid-svg-icons"
 import { faGears } from "@fortawesome/free-solid-svg-icons"
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons"
 import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
-import logo from "./images/nav-bar-logo.png"
+import logo from "./images/logo.png"
 
 
 const Navbar = () => {
@@ -15,11 +15,11 @@ const Navbar = () => {
         <div>
         <div>
             <nav className={open ? "nav" : "mini"} onMouseOver={() => setOpen(true)} onMouseOut={() => setOpen(false)}>
-                <h2><img src={logo} alt=""/> &nbsp;&nbsp;Parthiv</h2>
+                <h2><img src={logo} alt=""/> &nbsp;&nbsp;DTE Bank</h2>
                 <ul>
-                    <a href='#about'><li><FontAwesomeIcon className='icon' icon={faUser} /> About</li></a>
-                    <a href='#skills'><li><FontAwesomeIcon className='icon' icon={faGears} />Skills</li></a>
-                    <a href='#work'><li><FontAwesomeIcon className='icon' icon={faBriefcase} />Work</li></a>
+                    <a href='#about'><li><FontAwesomeIcon className='icon' icon={faUser} />  About</li></a>
+                    <a href='#services'><li><FontAwesomeIcon className='icon' icon={faGears} />Services</li></a>
+                    <a href='#login'><li><FontAwesomeIcon className='icon' icon={faBuildingColumns} /> Login</li></a>
                     <a href='#contact'><li><FontAwesomeIcon className='icon' icon={faAddressCard} />Contact</li></a>
                 </ul>
             </nav>
